376d71e2f85726b0fad932222752820983c8c016
zibbini <ziadibbini@gmail.com>
2021-02-03 12:20:24 +0000

    Fix bad merge

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 from setuptools import setup
 import codecs
 import os.path
-<<<<<<< HEAD
 import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
-=======
->>>>>>> 9879b60d5eedb23e8e6d6141b9da6c0ccef700b9
 
 # For reading in the version string without importing the package
 # Ref: https://packaging.python.org/guides/single-sourcing-package-version/
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
-<<<<<<< HEAD
     with codecs.open(os.path.join(here, rel_path), "r") as fp:
         return fp.read()
 
@@ -21,25 +17,16 @@ def read(rel_path):
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
         if line.startswith("__version__"):
-=======
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
->>>>>>> 9879b60d5eedb23e8e6d6141b9da6c0ccef700b9
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     else:
         raise RuntimeError("Unable to find version string.")
 
-<<<<<<< HEAD
 
 setup(
     name="heartcv",
     version=get_version("heartcv/__init__.py"),
-    description="Computer vision utilities.",
+    description="A platform for the semi-automated extraction of various cardiac proxy traits from bioimaging footage of embryos.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/EmbryoPhenomics/heartcv",
@@ -68,30 +55,4 @@ setup(
     project_urls={
         "Source": "https://github.com/EmbryoPhenomics/heartcv/tree/main/heartcv"
     },
-=======
-setup(name='heartcv',
-      version=get_version('heartcv/__init__.py'),
-      license='MIT',
-      packages=[
-          'heartcv',
-          'heartcv.core',
-          'heartcv.gui',
-          'heartcv.util'
-      ],
-      install_requires=[
-        'numpy',
-        'cython',
-        'matplotlib',
-        'scipy',
-        'tqdm',
-        'opencv-python',
-        'pandas',
-        'more_itertools',
-        'dash==1.9.1',
-        'dash_daq',
-        'dataclasses',
-        'flask',
-        'dash_table==4.6.1',
-        'natsort']
->>>>>>> 9879b60d5eedb23e8e6d6141b9da6c0ccef700b9
 )
