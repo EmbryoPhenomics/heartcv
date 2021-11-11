3ddc9186875c2619e5d3dc7ef6c362c8a5eeb67f
zibbini <ziadibbini@gmail.com>
2021-11-11 10:44:19 +0000

    Add gui and plotting tests.

--- heartcv/tests/test_ampd.py
+++ heartcv/tests/test_ampd.py
@@ -1,6 +1,8 @@
 import pytest
 import numpy as np
 from scipy import signal
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 import heartcv as hcv
 
@@ -54,3 +56,79 @@ def test_peaks(expected_peaks, data):
 def test_minmax_scale(data):
     out = hcv.minmax_scale(data)
     assert out.min() == 0 and out.max() <= 1.0
+
+
+@pytest.fixture
+def ampd_components():
+    return hcv.heartcv._ampd(sinusoidal())
+
+
+@pytest.fixture
+def ampd_plot(ampd_components):
+    yield hcv.heartcv._ampd_plot(*ampd_components, show=False)
+
+    plt.close("all")
+
+
+class TestPlot:
+    @pytest.fixture(autouse=True)
+    def setup(self, ampd_plot):
+        self.fig = ampd_plot
+
+    def text_in_fig(self):
+        # Taken from xarray testing suite
+        return [t.get_text() for t in plt.gcf().findobj(mpl.text.Text)]
+
+    def test_num_axes(self):
+        assert 6 == len(self.fig.axes) # Includes colorbar
+
+    def test_lms_axes(self):
+        axes = self.fig.axes[0]
+
+        assert "Local maxima scalogram (LMS)" == axes.get_title() 
+        assert "Index" == axes.get_xlabel()
+        assert "Scale [No]" == axes.get_ylabel()
+        assert "a)" in self.text_in_fig()
+
+        assert axes.has_data()
+
+    def test_rowsums_axes(self):
+        axes = self.fig.axes[1]
+
+        assert "Row-wise summation of LMS" == axes.get_title() 
+        assert "Scale [No]" == axes.get_xlabel()
+        assert "b)" in self.text_in_fig()
+
+        assert axes.has_data()
+
+    def test_G_axes(self):
+        axes = self.fig.axes[2]
+
+        assert "Rescaled LMS" == axes.get_title() 
+        assert "Index" == axes.get_xlabel()
+        assert "Scale [No]" == axes.get_ylabel()
+        assert "c)" in self.text_in_fig()
+
+        assert axes.has_data()
+
+    def test_S_axes(self):
+        axes = self.fig.axes[3]
+
+        assert "Column-wise standard deviation \n of the rescaled LMS" == axes.get_title() 
+        assert "Index" == axes.get_xlabel()
+        assert "σ" == axes.get_ylabel()
+        assert "d)" in self.text_in_fig()
+
+        assert axes.has_data()
+
+    def test_peaks_axes(self):
+        axes = self.fig.axes[4]
+
+        assert "Detected peaks" == axes.get_title() 
+        assert "Index" == axes.get_xlabel()
+        assert "Amplitude" == axes.get_ylabel()
+        assert "e)" in self.text_in_fig()
+
+        assert axes.has_data()
+
+
--- heartcv/tests/test_example_video.py
+++ heartcv/tests/test_example_video.py
@@ -5,16 +5,26 @@ import glob
 import heartcv as hcv
 
 
-def test_example_video():
+@pytest.fixture
+def expected_result():
     expected_parameters = dict(resolution=(600, 600), fps=30, codec="MJPG")
     expected_length = 600
 
-    video = hcv.load_example_video()
+    return expected_length, expected_parameters
+
+
+@pytest.fixture
+def video():
+    yield hcv.load_example_video()
+
+    for file in glob.glob("./*.avi"):
+        os.remove(file)
+
+
+def test_example_video(expected_result, video):
+    expected_length, expected_parameters = expected_result
 
     for key in expected_parameters.keys():
         assert expected_parameters[key] == getattr(video, key)
 
     assert expected_length == len(video)
-
-    for file in glob.glob("./*.avi"):
-        os.remove(file)
